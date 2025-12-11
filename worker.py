58cdce3e8ffcbc8f6d19d558e8df5440ec3c609f
Aditi Naoroibam <159549754+aditinao@users.noreply.github.com>
2025-12-11 22:35:36 +0530

    Remove worker.py file

--- worker.py
+++ worker.py
@@ -1,14 +1,1 @@
-# worker.py
-import os
-from redis import Redis
-from rq import Worker, Queue, Connection
 
-REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-listen = ["quiz-jobs"]
-
-redis_conn = Redis.from_url(REDIS_URL)
-
-if __name__ == "__main__":
-    with Connection(redis_conn):
-        w = Worker(list(map(Queue, listen)))
-        w.work()
